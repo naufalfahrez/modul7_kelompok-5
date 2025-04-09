5220db18f54f9d6589af498a2e70bc7314d8c96c
RakaValrizqy <pendragonqword@gmail.com>
2025-04-09 16:15:29 +0700

    Menambahkan class DataMahasiswa_103022330096

---
+++ DataMahasiswa_103022330096.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_5
+{
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state{ get; set; }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+    public class Mahasiswa 
+    { 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public Address address { get; set; }
+        public List<Course> courses { get; set; }
+    }
+    class DataMahasiswa_103022330096
+    {
+        public void ReadJson()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "jurnal7_1_103022330096.json");
+
+            string JsonString = File.ReadAllText(filePath);
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(JsonString);
+
+            Console.WriteLine($"Name: {mhs.firstName} {mhs.lastName}");
+            Console.WriteLine($"Gender: {mhs.gender}");
+            Console.WriteLine($"Age: {mhs.age}");
+            Console.WriteLine($"Address: {mhs.address.streetAddress}, {mhs.address.city}, {mhs.address.state}");
+            Console.WriteLine($"Courses: ");
+            for (int i=0; i<mhs.courses.Count; i++)
+            {
+                Console.WriteLine($"{mhs.courses[i].code} - {mhs.courses[i].name}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul7_kelompok_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa_103022330096 mhs = new DataMahasiswa_103022330096();
+        mhs.ReadJson();
+    }
+}
